ca734b3fb06f79298956034d4e91d9bf35b06b7e
Yazin <yazin@amalinvest.com>
2024-11-25 17:41:55 +0300

    working parser

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
-from utils import fetch_and_decode_data
+from utils import fetch_and_decode_data, flatten_and_standardize
+import json
 
 app = Flask(__name__)
 
@@ -7,9 +8,9 @@ app = Flask(__name__)
 def get_data():
     try:
         decoded_data = fetch_and_decode_data()
-        return jsonify(decoded_data)
+        return jsonify(flatten_and_standardize(decoded_data['data']))
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- utils.py
+++ utils.py
@@ -7,6 +7,61 @@ from cache_manager import load_cache, save_cache
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+def flatten_and_standardize(data):
+    """
+    Flattens and standardizes the input JSON data into a list of JSON entries 
+    representing companies and their associated data.
+    """
+
+    companies = []
+    # Process Sheet1 data
+    for company_data in data['Sheet1']:
+        # Get all sources in order
+        sources = []
+        source_fields = ['Source', 'Second source', 'Information source 3', 'Information source 4']
+        for field in source_fields:
+            value = company_data['data'].get(field, '')
+            if value:
+                sources.append(value)
+
+        company = {
+            'companyName': company_data['data']['Company Name'],
+            'companyId': company_data['data']['Company name'],
+            'sector': company_data['data']['Sector'],
+            'complicityDetails': company_data['data']['Complicity details'],
+            'recordLastUpdated': company_data['data']['Record last updated']['repr'],
+            'sources': sources,  # Add sources array
+        }
+        #Add complicity categories. Note that some categories may be absent.
+        for category in ["Military", "Settlement production", "Population control", "Economic exploitation", "Cultural"]:
+            if category in company_data['data']:
+                company[category.lower().replace(' ', '_')] = company_data['data'][category]
+        companies.append(company)
+
+
+    # Process Campaigns data, adding campaign-related information to companies
+    for campaign_data in data['Campaigns']:
+        print(campaign_data['data']['Companies'])
+        company_ids = [x.strip() for x in campaign_data['data']['Companies'].split(',')] #Handle potential multiple companies
+        for company_id in company_ids:
+            for i, company in enumerate(companies):
+                if company['companyId'] == company_id:
+                    campaign_info = campaign_data['data']
+                    company['campaignName'] = campaign_info.get('Campaign Name', '')
+                    company['campaignId'] = campaign_data['id']
+                    company['campaignDescription'] = campaign_info.get('Description', '')
+                    company['campaignLocation'] = campaign_info.get('Location', '')
+                    company['campaignOutcomes'] = campaign_info.get('Outcomes', '')
+                    company['campaignAimsAchieved'] = campaign_info.get('Aims achieved', '')
+                    company['campaignGroups'] = campaign_info.get('Campaign Groups', '')
+                    company['campaignMethods'] = campaign_info.get('9f119b48c6e3251dc6be2ae8a8b969c4', '')
+                    campaign_links = campaign_info.get('Campaign link', {}).get('$arrayItems', [])
+                    company['campaignLinks'] = [item for item in campaign_links if item]
+                    company['targetAim'] = campaign_info.get('Target aim: Divestment,Contract,Sponsor,Supply,Operations,Position,Other', '')
+                    break
+
+    return companies
+
 def fetch_and_decode_data():
     # Try to get data from cache first
     cached_data = load_cache()
